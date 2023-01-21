af916c5dc0472f261b103f569812bb69b7a48e71
Recibos <recibosalvaro@gmail.com>
2023-01-20 20:17:39 -0500

    feat(36-directive-environtment) variable env

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -13,30 +13,24 @@
     <p>        
         este es un parrafo
     </p>
-    
+
     {{--     
-        @if (false)
-            <p>
-                Este mensaje se mostrará si le pasamos el valor de verdadero a la condicional
-            </p>
-        @elseif(true)
-            <p>
-                Este mensaje se mostrara si le pasamos el valor de falso a la condicional
-            </p>
-        @endif
+        @env('local')
+            <p>Estamos en local</p>
+        @endenv
+
+        @env('production')
+            <p>Estamos en produccion</p>    
+        @endenv 
     --}}
 
-    {{-- 
-        @isset($prueba)
-            <p>La variable prueba se encuentra declarada</p>
-        @else
-            <p>La variable prueba no se encuentra declarada</p>
-        @endisset
+    @env('local')
+        <p>Estamos en local</p>
+    @endenv
 
-    --}}
+    @production
+        <p>Estamos en produccion</p>    
+    @endproduction
 
-    @empty($prueba)
-        la Variable prueba es nula
-    @endempty
 </body>
 </html>
