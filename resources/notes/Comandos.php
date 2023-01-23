f2502fe65440820f9fd362ec9290db006bcb61ac
ahuf <recibosalvaro@gmail.com>
2023-01-22 20:25:08 -0500

    feat(53-foreing-keys) add new tables: posts, tags and post_tag

--- resources/notes/Comandos.php
+++ resources/notes/Comandos.php
@@ -17,3 +17,5 @@ php artisan migrate:reset             //Elimina todas las migraciones existentes
 php artisan migrate:refresh           //Ejecuta el metodo down() y el metodo up() eliminando y creando nuevamente todas las tablas exepto las que se crearon sin migracion (manualmente)
 php artisan migrate:fresh             //Elimina todas las tablas incluso las que se crearon sin mogracion  
 
+crear migracion
+php artisan make:migration create_posts_table //crea una migracion y define los metodos up() y down con el nombre posts
