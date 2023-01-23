58e6c8e84f107ffb8cab82af4b14bdf2e3142170
ahuf <recibosalvaro@gmail.com>
2023-01-22 22:39:12 -0500

    feat(57-indices) add new index methods

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,8 +15,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            // $table->unsignedBigInteger('id')
+            //       ->autoIncrement();
+
+            // $table->string('name');
+                  
+            // $table->primary(['id', 'name']); asignar llave primaria compuesta
+
+            // crear indices para hacer mas rapidas las consultas
+
+            // $table->string('name')->index();
+            // $table->index('name');
+
+            // Agregar indice fulltext
+            $table->string('name')->fulltext();
+            $table->dropFullText('name');
+
             $table->string('email')->unique();
+            // $table->unique('email'); definir el campo y luego asignando un valor de tipo unico
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
