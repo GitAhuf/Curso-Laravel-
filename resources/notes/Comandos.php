aaf9893f32d12bae8e292113562b86e1074f45f2
ahuf <recibosalvaro@gmail.com>
2023-01-22 19:41:00 -0500

    feat(52-revert-change-migrations) how create and delete migrations

--- resources/notes/Comandos.php
+++ resources/notes/Comandos.php
@@ -6,3 +6,14 @@ php artisan r:l                     //Lista todas las rutas del proyecto
 php artisan r:l --name=posts        //Lista las rutas con el nombre posts 
 php artisan make:controller PruebaController --resource
 php artisan make:controller AboutController --resource
+
+php artisan make:migration categories //crea un archivo de migracion con el nombre  que se le ha enviado
+php artisan migrate                   //Ejecuta una nueva migracion
+
+Metodos destructivos
+php artisan migrate:rollback          //Revierte la ultima migracion ejecutada por lotes
+php artisan migrate:rollback --step=2 //Revierte las dos ultimas migraciones ejecutadas sin importar el lote
+php artisan migrate:reset             //Elimina todas las migraciones existentes
+php artisan migrate:refresh           //Ejecuta el metodo down() y el metodo up() eliminando y creando nuevamente todas las tablas exepto las que se crearon sin migracion (manualmente)
+php artisan migrate:fresh             //Elimina todas las tablas incluso las que se crearon sin mogracion  
+
