c72bf8f63e3bd959d321a159a47827a4a1ebd1ab
ahuf <recibosalvaro@gmail.com>
2023-04-19 12:37:47 -0500

    agregando estilos de bootstrap al paginador

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
@@ -24,6 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Paginator::useBootstrapFive();
         View::composer(['posts.index','posts.create', 'posts.show'], 'App\View\Composers\PostComposer');
         View::share('prueba', 'Este es el mensaje de prueba que estara disponible en todas las vistas');
     }
